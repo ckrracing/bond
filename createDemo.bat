0ed6cf932bb9370984bd66d797c1edc5240e87a2
ckrracing <netadmin@thisdigitalife.net>
2017-02-02 16:30:37 +1000

    Add files via upload

---
+++ createDemo.bat
@@ -0,0 +1,17 @@
+call "callPS2EXE.bat" "C:\Users\Public\Documents\PS2EXE-v0.5.0.0\vc\bond\functions.ps1" "bond_vbi_x64.exe" -iconFile PSEXE.ico
+rem call "callPS2EXE.bat" "C:\Users\Public\Documents\WriteOptions_1.ps1" "bond_vbi_wo_x64.exe" -iconFile PSEXE.ico
+rem call "callPS2EXE.bat" "C:\Users\Public\Documents\Veeam-Summary-Bwiki-Integration.ps1" "bond_vbi_x64.exe" -iconFile PSEXE.ico
+
+rem call "callPS2EXE.bat" "C:\Users\Public\Documents\Veeam-Summary-Bwiki-Integration.ps1" "bond_vbi_x64.exe" -x64 
+
+rem call "callPS2EXE.bat" "test.ps1" "test_x86.exe" -x86
+
+rem call "callPS2EXE.bat" "test.ps1" "test_20_STA.exe" -sta -runtime20 -iconFile PS2EXE.ico
+
+rem call "callPS2EXE.bat" "test.ps1" "test_30_MTA.exe" -mta -runtime30
+
+rem call "callPS2EXE.bat" "test.ps1" "test_30_NOCONSOLE.exe" -noconsole -runtime30
+
+rem call "callPS2EXE.bat" "test.ps1" "test_20_NOCONSOLE.exe" -noconsole -runtime20
+
+rem call "callPS2EXE.bat" "test.ps1" "test_40.exe" -runtime40
